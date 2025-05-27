a11abc850da5c2ab9b2d69527aae7c4cbe5fc843
DouglasKuerten <dk.kuerten.dk@hotmail.com>
2025-05-27 19:33:29 -0300

    Docker compose files cleanup

--- server.js
+++ server.js
@@ -34,9 +34,9 @@ import {
 import { gerarUsuarioAdmin } from './src/scripts/gerarUsuarioAdmin.js';
 
 sequelize
-  .sync({ force: false })
+  .sync({ force: false, logging: false })
   .then(() => {
-    console.log('Todos os modelos foram sincronizados com sucesso');
+    console.debug('Todos os modelos foram sincronizados com sucesso');
     gerarUsuarioAdmin().catch((err) => console.error(err));
   })
   .catch((error) => {
@@ -59,10 +59,6 @@ app.use(
   }),
 );
 
-app.listen(port, () => {
-  console.log(`Servidor rodando na porta de conexão ${port}.`);
-});
-
 app.use('/api', routes);
 
 const upload = multer({ storage: storage });
@@ -75,5 +71,10 @@ app.use('/files', express.static('uploads'));
 process.on('SIGINT', function () {
   console.log('\nGracefully shutting down from SIGINT (Ctrl-C)');
   process.exit(0);
+});
+app.listen(port, () => {
+  console.info(`API disponível em http://localhost:${port}/api`);
+  console.info(`Swagger disponível em http://localhost:${port}/api/docs`,
+  );
 });
 app.use(errorHandler);
